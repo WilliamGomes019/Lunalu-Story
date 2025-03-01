10eb4f68358d15253cbfb593479c925919414524
ᾇηdŗeʑin <andre37341@fiec.edu.br>
2025-03-01 15:19:33 -0300

    form correction
    now it will show an alert before you send it the informations in the forms

--- js/contato.js
+++ js/contato.js
@@ -15,3 +15,17 @@ modoEscuroButton.addEventListener('click', () => {
         localStorage.setItem('darkMode', 'disabled');
     }
 });
+
+// Adiciona funcionalidade ao botão de envio do formulário
+document.addEventListener("DOMContentLoaded", function() {
+    const form = document.querySelector(".form-container form");
+    
+    form.addEventListener("submit", function(event) {
+        event.preventDefault(); // Impede o envio real do formulário
+        
+        alert("Mensagem enviada com sucesso!"); // Exibe o aviso
+        
+        // Limpa todos os campos do formulário
+        form.reset();
+    });
+});
